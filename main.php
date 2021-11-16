f3f76e8e31986c5aeadb393a875ee9e62c374a84
Markk007 <73937886+Markk007@users.noreply.github.com>
2021-11-16 16:36:07 +0800

    Add files via upload

---
+++ main.php
@@ -0,0 +1,257 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="img2/moymoylogo.png">
+  <title>Moy Moy's Party Need Store</title>
+  <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+  <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+  <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/navbar-fixed/">
+
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+  <!------ Include the above in your HEAD tag ---------->
+
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.css" media="screen">
+  <script src="//cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
+
+  <!-- Bootstrap core CSS -->
+  <link href="css\bootstrap.min.css" rel="stylesheet">
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+      }
+    }
+  </style>
+
+
+  <!-- Custom styles for this template -->
+</head>
+
+<body>
+  <link href="css\navbar-top-fixed.css" rel="stylesheet">
+
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" style="font-size: 15px;">
+    <div class="container-fluid">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+        aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="main.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Shop</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="aboutus.php">About Us</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+
+
+  <!--HEROES-->
+
+
+  <link href="css\heroes.css" rel="stylesheet">
+  <link href="css\main_carousel.css" rel="stylesheet">
+
+  <main>
+
+    <div class="px-4 my-5 text-center">
+      <div>
+        <p class="display fw-bold">Moy Moy's Party Needs Store</p>
+      </div>
+    </div>
+
+    <div class="b-example-divider"></div>
+    <div class="slider">
+      <div class="slide-track">
+        <div class="slide">
+          <img src="img2\moy1.jpg" height="397" width="698">
+        </div>
+        <div class="slide">
+          <img src="img2\moy2.jpg" height="397" width="698">
+        </div>
+        <div class="slide">
+          <img src="img2\moy3.jpg" height="397" width="698">
+        </div>
+        <div class="slide">
+          <img src="img2\moy4.jpg" height="397" width="698">
+        </div>
+        <div class="slide">
+          <img src="img2\moy5.jpg" height="397" width="698">
+        </div>
+        <div class="slide">
+          <img src="img2\moy2.jpg" height="397" width="698">
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="b-example-divider"></div>
+
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+
+        <div class="container page-top">
+          <div class="row">
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="balloons.php">
+                <img src="img2\1.JPG" class="zoom img-fluid ">
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="lootbags.php">
+                <img src="img2\10.JPG" class="zoom img-fluid ">
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="decoration.php">
+                <img src="img2\8.JPG" class="zoom img-fluid ">
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="miscellaneous.php">
+                <img src="img2\6.JPG" class="zoom img-fluid ">
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="balloons.php">
+                <img src="img2\3.JPG" class="zoom img-fluid ">
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="lootbags.php">
+                <img src="img2\9.JPG" class="zoom img-fluid ">
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="decoration.php">
+                <img src="img2\11.JPG" class="zoom img-fluid ">
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="miscellaneous.php">
+                <img src="img2\18.JPG" class="zoom img-fluid ">
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="balloons.php">
+                <img src="img2\4.JPG" class="zoom img-fluid ">
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="lootbags.php">
+                <img src="img2\21.JPG" class="zoom img-fluid ">
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="decoration.php">
+                <img src="img2\17.JPG" class="zoom img-fluid ">
+              </a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-xs-6 thumb">
+              <a href="miscellaneous.php">
+                <img src="img2\20.JPG" class="zoom img-fluid ">
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="disp1 col-lg-6">
+        <h1 class="display-5 fw-bold lh-1 mb-3">Inside Our Store</h1>
+        <p class="lead">Here are few of the products inside <span>Moy Moy's Party Needs Store</span>! <br> Feel free to
+          browse through our shop!!</p>
+      </div>
+    </div>
+
+
+    <link href="css\main_style4.css" rel="stylesheet">
+    <div class="b-example-divider"></div>
+    <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="footer-col">
+            <h4>company</h4>
+            <ul>
+              <li><a href="#">about us</a></li>
+              <li><a href="#">our services</a></li>
+              <li><a href="#">privacy policy</a></li>
+              <li><a href="#">affiliate program</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>get help</h4>
+            <ul>
+              <li><a href="#">FAQ</a></li>
+              <li><a href="#">shipping</a></li>
+              <li><a href="#">returns</a></li>
+              <li><a href="#">order status</a></li>
+              <li><a href="#">payment options</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>Our Shop</h4>
+            <ul>
+              <li><a href="#">balloons</a></li>
+              <li><a href="#">bags</a></li>
+              <li><a href="#">hats</a></li>
+              <li><a href="#">tables and chair</a></li>
+              <li><a href="#">miscellaneous</a></li>
+            </ul>
+          </div>
+          <div class="footer-col">
+            <h4>follow us</h4>
+            <div class="social-links">
+              <a class="btn" href="https://www.facebook.com/Moymoy-party-needs-tarpaulin-printing-925016044220883"
+                target="_blank">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a class="btn" href="#">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a class="btn" href="#">
+                <i class="fab fa-google"></i>
+              </a>
+              <a class="btn" href="#">
+                <i class="fab fa-instagram"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </main>
+
+
+  <script src="js/bootstrap.bundle.min.js"></script>
+  <script src="js/photoGrid.js"></script>
+
+
+</body>
+
+</html>
